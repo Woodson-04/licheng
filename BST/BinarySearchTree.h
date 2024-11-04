688989fd0f8c9eb62f13ed37ddc99d910051f7ee
Woodson-04 <3230103495@zju.edu.cn>
2024-11-05 00:51:24 +0800

    Add BST folder with its contents

---
+++ BST/BinarySearchTree.h
@@ -0,0 +1,242 @@
+#include <iostream>
+
+class UnderflowExcepection {};
+
+template <typename Comperable>
+class BinarySearchTree
+{
+public:
+    BinarySearchTree(): root{nullptr}{}
+    BinarySearchTree(const BinarySearchTree &rhs): root{clone(rhs.root)}{}
+    BinarySearchTree(BinarySearchTree &&rhs) noexcept : root{rhs.root}
+    {
+        rhs.root = nullptr;
+    }
+    ~BinarySearchTree()
+    {
+        makeEmpty();
+    }
+    const Comperable &findMin() const
+    {
+        if (isEmpty())
+            throw UnderflowExcepection{};
+        return findMin(root)->element;
+    }
+    const Comperable &findMax() const
+    {
+        if (isEmpty())
+            throw UnderflowExcepection{};
+        return findMax(root)->element;
+    }
+    bool contains(const Comperable &x) const
+    {
+        return contains(x, root);
+    }
+    bool isEmpty() const
+    {
+        return root == nullptr;
+    }
+    void printTree(std::ostream &out = std::cout) const
+    {
+        if (isEmpty())
+        {
+            out << "Empty tree" << std::endl;
+        }
+        else
+        {
+            printTree(root, out);
+        }
+    }
+    void makeEmpty()
+    {
+        makeEmpty(root);
+    }
+    void insert(const Comperable &x)
+    {
+        insert(x, root);
+    }
+    void insert(Comperable &&x)
+    {
+        insert(std::move(x), root);
+    }///
+    void remove(const Comperable &x)
+    {
+        remove(x, root);
+    }
+    BinarySearchTree &operator=(const BinarySearchTree &rhs)
+    {
+        if (this != &rhs)
+        {
+            BinarySearchTree temp(rhs);
+            std::swap(root, temp.root);
+        }
+        return *this;
+    }
+    BinarySearchTree &operator=(BinarySearchTree &&rhs) noexcept
+    {
+        std::swap(root, rhs.root);
+        return *this;
+    }
+
+private:
+    struct BinaryNode
+    {
+        Comperable element;
+        BinaryNode *left;
+        BinaryNode *right;
+
+        BinaryNode(const Comperable &theElement, BinaryNode *lt, BinaryNode *rt)
+            :element{theElement}, left{lt}, right{rt}{}
+        BinaryNode(Comperable &&theElement, BinaryNode *lt, BinaryNode *rt)
+            :element{std::move(theElement)}, left{lt}, right{rt}{}
+    };
+    BinaryNode *root;
+
+    BinaryNode *findMin(BinaryNode *t)const
+    {
+        if (t == nullptr)
+        {
+            return nullptr;
+        }
+        if (t->left == nullptr)
+        {
+            return t;
+        }
+        return findMin(t->left);
+    }
+    BinaryNode *findMax(BinaryNode *t) const
+    {
+        if (t != nullptr)
+        {
+            while (t->right != nullptr)
+            {
+                t = t->right;
+            }
+        }
+        return t;
+    }
+    bool contains(const Comperable &x, BinaryNode *t) const
+    {
+        if (t == nullptr)
+        {
+            return false;
+        }
+        if (x < t->element)
+        {
+            return contains(x, t->left);
+        }
+        else if (x > t->element)
+        {
+            return contains(x, t->right);
+        }
+        else
+        {
+            return true;
+        }
+    }
+    void printTree(BinaryNode *t, std::ostream &out) const
+    {
+        if (t != nullptr)
+        {
+            printTree(t->left, out);
+            out << t->element << std::endl;
+            printTree(t->right, out);
+        }
+    }
+    void makeEmpty(BinaryNode *&t)
+    {
+        ///这必须是一个后序遍历，为什么？
+        if (t != nullptr)
+        {
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
+            t = nullptr;
+        }
+    }
+    void insert(const Comperable &x, BinaryNode *&t)
+    {
+        if (t == nullptr)
+        {
+            t = new BinaryNode{x, nullptr, nullptr};
+        }
+        else if (x < t->element)
+        {
+            insert(x, t->left);
+        }
+        else if (x > t->element)
+        {
+            insert(x, t->right);
+        }
+        else
+        {
+            ///这种情况不可省略（如果元素已存在，则不进行插入）
+        }
+    }
+    void insert(Comperable &&x, BinaryNode *&t)
+    {
+        if (t == nullptr)
+        {
+            t = new BinaryNode{std::move(x), nullptr, nullptr};
+        }
+        else if (x < t->element)
+        {
+            insert(std::move(x), t->left);
+        }
+        else if(x > t->element)
+        {
+            insert(std::move(x), t->right);
+        }
+        else
+        {
+
+        }
+    }
+    BinaryNode *clone(BinaryNode *t) const
+    {
+        if (t== nullptr)
+        {
+            return nullptr;
+        }
+        return new BinaryNode{t->element, clone(t->left), clone(t->right)};
+    }
+    void remove(const Comperable &x, BinaryNode *&t) 
+    {
+        if (t == nullptr) 
+        {
+            return;
+        }
+        if (x < t->element)
+        {
+            remove(x, t->left);
+        } 
+        else if (x > t->element) 
+        {
+            remove(x, t->right);
+        }
+        else 
+        {
+            if (t->left != nullptr && t->right != nullptr)
+            {
+                BinaryNode *minNode = detachMin(t->right);
+                t->element = minNode->element;
+                delete minNode;
+            } 
+            else 
+            {
+                BinaryNode *temp = t;
+                t = (t->left != nullptr) ? t->left : t->right;
+                delete temp;
+            }
+        }
+    }
+    BinaryNode *detachMin(BinaryNode *&t)
+    {
+    if (t->left == nullptr) {
+        BinaryNode *temp = t;
+        t = t->right;
+        return temp;
+    }
+    return detachMin(t->left);
+    }
+};
---
+++ BST/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "BinarySearchTree.h"  // 假设 BinarySearchTree 类定义在这个头文件中
+
+void testBinarySearchTree() {
+    BinarySearchTree<int> bst;
+
+    // 测试插入功能
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(15);
+    bst.insert(3);
+    bst.insert(7);
+    bst.insert(12);
+    bst.insert(18);
+
+    bst.remove(7);
+    std::cout << "Tree after removing 7:" << std::endl;
+    bst.printTree();
+
+    bst.remove(10);
+    std::cout << "Tree after removing 10:" << std::endl;
+    bst.printTree();
+
+    bst.remove(20);
+    std::cout << "Tree after removing 10:" << std::endl;
+    bst.printTree();
+
+}
+
+int main() {
+    testBinarySearchTree();
+    return 0;
+}
